f52a6207e4e2be3194807ee1d88404085ab8b57c
강현지 <hyeonji.2>
2019-05-04 18:13:58 +0900

    재료 형태소 분석

--- Test/src/HangleAnalyze.java
+++ Test/src/HangleAnalyze.java
@@ -1,77 +1,89 @@
 
-
 import java.util.List;
-import org.snu.ids.kkma.index.Keyword;
-import org.snu.ids.kkma.index.KeywordExtractor;
-import org.snu.ids.kkma.index.KeywordList;
+
 import org.snu.ids.kkma.ma.CharSetType;
 import org.snu.ids.kkma.ma.MExpression;
+import org.snu.ids.kkma.ma.Morpheme;
 import org.snu.ids.kkma.ma.MorphemeAnalyzer;
 import org.snu.ids.kkma.ma.Sentence;
 
 public class HangleAnalyze {
-	
+
 	private static class HangleAnalyzeLazy {
 		public static final HangleAnalyze LAZY_INSTANCE = new HangleAnalyze();
 	}
-	
-	public static HangleAnalyze getInstance()
-	{
+
+	public static HangleAnalyze getInstance() {
 		return HangleAnalyzeLazy.LAZY_INSTANCE;
 	}
-	
-	private MorphemeAnalyzer ma;
-	
-	public void initilze()
-	{
-		ma = new MorphemeAnalyzer();
-        ma.createLogger(null);
-	}
-	
-	public void analyze(String content)
-	{
-		try {
-            List<MExpression> ret = ma.analyze(content);
-            
-            ret = ma.postProcess(ret);
-            ret = ma.leaveJustBest(ret);
-            
-            List<Sentence> stl = ma.divideToSentences(ret);
-            for( int i = 0; i < stl.size(); i++ ) {
-                  Sentence st = stl.get(i);
-                  System.out.println("=============================================  " + st.getSentence());
-                  for( int j = 0; j < st.size(); j++ ) {
-                        System.out.println(st.get(j));
-                  }
-                  
-                  System.out.println("=============================================");
-            }
-      } catch (Exception e) {
-            e.printStackTrace();
-      }
-	}
-	
-	public void getKeyWord(String content)
-	{
-		System.out.println("=============================================");
-		
-		// init KeywordExtractor
-		KeywordExtractor ke = new KeywordExtractor();
-		// extract keywords
-		KeywordList kl = ke.extractKeyword(content, true);
-		// print result
-		for( int i = 0; i < kl.size(); i++ ) {
-		    Keyword kwrd = kl.get(i);
-		   /* if(kwrd.getComposed().equals("C") )
-		    {
-		    	System.out.println(kwrd.getString());
-		    } else if(kwrd.getCharSet().equals("S"))
-		    {
-		    	System.out.println(kwrd.getString());
-		    }*/
-		   System.out.println(kwrd.getString() + "\t" + kwrd.getSmplStr() + "\t" + kwrd.getSmplStr2() + "\t"+ kwrd.getCharSetName() + "\t" + kwrd.getComposed() + "\t" + kwrd.getCharSet() + "\t" + kwrd.getFreq() + "\t" + kwrd.getTag() + "\t" + kwrd.getVocTag() );
-		}
-		System.out.println("=============================================");
 
+	private MorphemeAnalyzer ma;
+
+	public void initilze() {
+		ma = new MorphemeAnalyzer();
+		ma.createLogger(null);
 	}
+
+	public void analyze(String content) {
+		try {
+			List<MExpression> ret = ma.analyze(content);
+
+			ret = ma.postProcess(ret);
+			ret = ma.leaveJustBest(ret);
+
+			List<Sentence> stl = ma.divideToSentences(ret);
+			for (int i = 0; i < stl.size(); i++) {
+				Sentence st = stl.get(i);
+
+				System.out.println("=============================================  " + st.getSentence());
+
+				for (int j = 0; j < st.size(); j++) {
+					for (int k = 0; k < st.get(j).size(); k++) {
+						Morpheme mo = st.get(j).get(k);
+						String charStr = mo.getString();
+						String tagStr = mo.getTag();
+						// System.out.println(charStr+", "+tagStr);
+
+						if(mo.getCharSet() == CharSetType.NUMBER)
+						{
+							if(k + 1 < st.get(j).size() )
+							{
+								Morpheme nextMo = st.get(j).get(k + 1);
+								System.out.println(mo.getString());
+								System.out.println(nextMo.getString());
+
+							}
+
+						}
+						/*
+						System.out.println(mo.toString());
+						if (tagStr.equals("NR") || tagStr.equals("OL")) {
+							System.out.println(charStr);
+						}
+						if (tagStr.equals("NNG") || tagStr.equals("UN")) {
+							if (j - 1 > 0) {
+								if (st.get(j - 1).get(k).getTag().equals("NR")) {
+									System.out.println(charStr);
+								}
+							}
+						}*/
+					}
+					/*
+					 * for(int k = 0; k< ss.length ; k++) { String str[] = ss[k].split("\\/");
+					 * String charStr = str[0]; String tagStr = str[1]; if(tagStr.equals("NR")) {
+					 *
+					 * //String strr[] = ss[k+1].split("\\/"); //if(strr[1].equals(")) }
+					 *
+					 * }
+					 */
+
+				}
+
+				System.out.println("=============================================");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
 }
