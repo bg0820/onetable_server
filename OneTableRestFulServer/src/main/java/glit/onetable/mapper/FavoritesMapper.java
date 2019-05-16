7999a7a8ce744a926097866a4e29a2a32fdc0921
강현지 <hyeonji.2>
2019-05-16 17:08:40 +0900

    Auto stash before merge of "master" and "origin/master"

---
+++ OneTableRestFulServer/src/main/java/glit/onetable/mapper/FavoritesMapper.java
@@ -0,0 +1,8 @@
+package glit.onetable.mapper;
+
+import glit.onetable.model.vo.Favorites;
+
+public interface FavoritesMapper {
+	public int favoritesInsert(Favorites favorites);
+
+}
---
+++ OneTableRestFulServer/src/main/java/glit/onetable/model/vo/Favorites.java
@@ -0,0 +1,35 @@
+package glit.onetable.model.vo;
+
+public class Favorites {
+	int userIdx;
+	int favoriteIdx;
+	int favoritesType;
+	int alterPrice;
+
+	public int getUserIdx() {
+		return userIdx;
+	}
+	public void setUserIdx(int userIdx) {
+		this.userIdx = userIdx;
+	}
+	public int getFavoriteIdx() {
+		return favoriteIdx;
+	}
+	public void setFavoriteIdx(int favoriteIdx) {
+		this.favoriteIdx = favoriteIdx;
+	}
+	public int getFavoritesType() {
+		return favoritesType;
+	}
+	public void setFavoritesType(int favoritesType) {
+		this.favoritesType = favoritesType;
+	}
+	public int getAlterPrice() {
+		return alterPrice;
+	}
+	public void setAlterPrice(int alterPrice) {
+		this.alterPrice = alterPrice;
+	}
+
+
+}
