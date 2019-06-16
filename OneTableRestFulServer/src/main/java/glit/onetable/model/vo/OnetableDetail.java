6d6509fb571a23bf79ec36bee6520263976645ee
kimbogyu <bg0820@naver.com>
2019-06-16 22:37:10 +0900

    한상차림 디테일 추가 및 히스토리 수정

---
+++ OneTableRestFulServer/src/main/java/glit/onetable/model/vo/OnetableDetail.java
@@ -0,0 +1,76 @@
+package glit.onetable.model.vo;
+
+public class OnetableDetail {
+	private int onetablesetIdx;
+	private int recipeIdx;
+	private String recipeName;
+	private String recipeImg;
+	private String nickname;
+	private int price;
+	private double x;
+	private double y;
+
+	public int getOnetablesetIdx() {
+		return onetablesetIdx;
+	}
+
+	public void setOnetablesetIdx(int onetablesetIdx) {
+		this.onetablesetIdx = onetablesetIdx;
+	}
+
+	public int getRecipeIdx() {
+		return recipeIdx;
+	}
+
+	public void setRecipeIdx(int recipeIdx) {
+		this.recipeIdx = recipeIdx;
+	}
+
+	public String getRecipeName() {
+		return recipeName;
+	}
+
+	public void setRecipeName(String recipeName) {
+		this.recipeName = recipeName;
+	}
+
+	public String getRecipeImg() {
+		return recipeImg;
+	}
+
+	public void setRecipeImg(String recipeImg) {
+		this.recipeImg = recipeImg;
+	}
+
+	public String getNickname() {
+		return nickname;
+	}
+
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	public double getX() {
+		return x;
+	}
+
+	public void setX(double x) {
+		this.x = x;
+	}
+
+	public double getY() {
+		return y;
+	}
+
+	public void setY(double y) {
+		this.y = y;
+	}
+}
